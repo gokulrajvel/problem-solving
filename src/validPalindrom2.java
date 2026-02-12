8b009dcfb9cba258ff245717517335ea79ec4c93
Gokulraj Velu <vgokulraj360@gmail.com>
2026-02-12 17:29:23 +0530

    valid palindrom2

---
+++ src/validPalindrom2.java
@@ -0,0 +1,28 @@
+public class validPalindrom2 {
+    public boolean validPalindrome(String s) {
+        int start=0,end=s.length()-1;
+        while(start<end){
+
+            if(s.charAt(start) == s.charAt(end))
+            {
+                start++;
+                end--;
+            }
+            else
+                return palindrome(s,start+1,end)||palindrome(s,start,end-1);
+        }
+        return true;
+    }
+    boolean palindrome(String s,int start,int end){
+        while(start<end){
+            if(s.charAt(start)!=s.charAt(end))  return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        validPalindrom2 p = new validPalindrom2();
+        System.out.print(p.validPalindrome("abca"));
+    }
+}

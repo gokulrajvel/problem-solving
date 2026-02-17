ff51a78310d0dfba8a486a3f7e9cb77d8386a8bd
Gokulraj Velu <vgokulraj360@gmail.com>
2026-02-17 17:39:46 +0530

    easy

---
+++ src/Bsearch.java
@@ -0,0 +1,25 @@
+public class Bsearch {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={-1,0,3,5,9,12};
+        int target=9;
+        Bsearch bsearch=new Bsearch();
+        System.out.println(bsearch.search(arr,target));
+    }
+}

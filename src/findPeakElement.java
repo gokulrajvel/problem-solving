c4e3498f9dae07190e396a7e0eeb8d617f966758
Gokulraj Velu <vgokulraj360@gmail.com>
2026-02-17 17:24:00 +0530

    medium

---
+++ src/findPeakElement.java
@@ -0,0 +1,20 @@
+public class findPeakElement {
+    public static void main(String[] args) {
+        findPeakElement findpeak=new findPeakElement();
+        int[] arr={1,2,3,1};
+        int index=findpeak.findPeakElements(arr);
+        System.out.println("index:"+index+" value:"+arr[index]);
+    }
+    public int findPeakElements(int[] nums) {
+        int start=0,end=nums.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[mid+1]){
+                end=mid;
+            }
+            else
+                start=mid+1;
+        }
+        return end;
+    }
+}

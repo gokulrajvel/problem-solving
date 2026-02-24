8727c28816a6c7b14ed2b3cf1809069ea7e87493
Gokulraj Velu <115938280+gokulrajvel@users.noreply.github.com>
2026-02-24 15:24:28 +0530

    Refactor numSubarrayProductLessThanK method parameters

--- src/subarrayProduct.java
+++ src/subarrayProduct.java
@@ -3,17 +3,17 @@ public class subarrayProduct {
         subarrayProduct obj=new subarrayProduct();
         System.out.println(obj.numSubarrayProductLessThanK(new int[]{10,5,2,6}, 100));
     }
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if(k<=1)
+    public int numSubarrayProductLessThanK(int[] nums, int target) {
+        if(target<=1)
             return 0;
-        int count=0,left=0,right=0,product=1;
+        int result=0,left=0,right=0,product=1;
         while(right<nums.length){
             product *= nums[right];
-            while(product>=k)
+            while(product>=target)
                 product/=nums[left++];
-            count+=1+(right-left);
+            result+=1+(right-left);
             right++;
         }
-        return count;
+        return result;
     }
 }

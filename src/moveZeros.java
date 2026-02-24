857c32a4f3ebbf17eb9c5671378fec9f1d162975
Gokulraj Velu <115938280+gokulrajvel@users.noreply.github.com>
2026-02-24 15:01:59 +0530

    Fix zero movement logic in moveZeroes method

--- src/moveZeros.java
+++ src/moveZeros.java
@@ -2,14 +2,14 @@ import java.util.Arrays;
 
 public class moveZeros {
     public int[] moveZeroes(int[] nums) {
-        int k=0;
+        int index=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0)
             {
-                nums[k++]=nums[i];
+                nums[index++]=nums[i];
             }
         }
-        for(int i=k;i<nums.length;i++)
+        for(int i=index;i<nums.length;i++)
             nums[i]=0;
         return nums;
     }

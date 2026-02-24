f4d4e2916255bafbf1b3d546ca5a4d289d6b692e
Gokulraj Velu <115938280+gokulrajvel@users.noreply.github.com>
2026-02-24 14:35:54 +0530

    Refactor bagOfTokens class and variable names

--- src/bagOfTokens.java
+++ src/bagOfTokens.java
@@ -1,20 +1,20 @@
 public class bagOfTokens {
     public static void main(String[] args){
-        bagOfTokens a=new bagOfTokens();
-        System.out.println(a.bagOfToken(new int[]{100,200,300,400},200));
+        bagOfTokens obj=new bagOfTokens();
+        System.out.println(obj.bagOfToken(new int[]{100,200,300,400},200));
     }
     public int bagOfToken(int [] arr,int target){
         int left=0,right=arr.length-1;
-        int max=0,val=0;
+        int max=0,score=0;
         while(left<=right){
             if(arr[left]<=target){
                 target-=arr[left++];
-                val++;
-                max=Math.max(max,val);
+                score++;
+                max=Math.max(max,score);
             }
-            else if(val>=1){
+            else if(score>=1){
                 target+=arr[right--];
-                val--;
+                score--;
             }
             else  break;
         }

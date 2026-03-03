57755d1c642d802abbcd87b0c3b621457e38dd90
Gokulraj Velu <vgokulraj360@gmail.com>
2026-03-03 11:22:59 +0530

    medium 165. Compare Version Numbers

--- src/CompareVersionNumbers.java
+++ src/CompareVersionNumbers.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class CompareVersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int len = Math.max(v1.length, v2.length);
+        for (int i = 0; i < len; i++) {
+            int val1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int val2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if (val1 > val2)
+                return 1;
+            if (val1 < val2)
+                return -1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        CompareVersionNumbers obj=new CompareVersionNumbers();
+        System.out.println(obj.compareVersion("1.1.1.1", "1.1.1.1"));
+    }
 }

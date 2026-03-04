be09216e774e530d5c4e22cb1d802e3067b488db
Gokulraj Velu <vgokulraj360@gmail.com>
2026-03-04 09:28:02 +0530

    Medium 6. Zigzag Conversion

--- src/ZigzagConversion.java
+++ src/ZigzagConversion.java
@@ -2,31 +2,31 @@ import java.util.Scanner;
 
 public class ZigzagConversion {
     public String convert(String s, int numRows) {
-        if(numRows<=1||s.length()<=numRows)
+        if (numRows <= 1 || s.length() <= numRows)
             return s;
-        String str="";
-        String[] arr=new String[numRows];
-        for(int i=0;i<arr.length;i++)
-            arr[i]="";
-        int index=0;
-        boolean down=true;
-        for(char g:s.toCharArray()){
-            arr[index]+=String.valueOf(g);
-            if(index==0)
-                down=true;
-            else if(index==numRows-1)
-                down=false;
-            index+=down?1:-1;
+        String str = "";
+        String[] arr = new String[numRows];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = "";
+        int index = 0;
+        boolean down = true;
+        for (char g : s.toCharArray()) {
+            arr[index] += String.valueOf(g);
+            if (index == 0)
+                down = true;
+            else if (index == numRows - 1)
+                down = false;
+            index += down ? 1 : -1;
         }
-        for(String st:arr)
-            str+=st;
+        for (String st : arr)
+            str += st;
         return str;
     }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        ZigzagConversion obj=new ZigzagConversion();
-        System.out.println(obj.convert(s,4));
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        ZigzagConversion obj = new ZigzagConversion();
+        System.out.println(obj.convert(s, 4));
     }
 }

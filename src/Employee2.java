94e2ce9bca32582d396c99309e47688cbb7d463e
Gokulraj Velu <vgokulraj360@gmail.com>
2026-03-07 11:16:06 +0530

    update

--- src/Employee2.java
+++ src/Employee2.java
@@ -2,26 +2,35 @@ import java.util.Scanner;
 
 public class Employee2 {
     public static void main(String[] args) {
-        EmployeeDetails2 obj=new EmployeeDetails2();
-        String[] emp=obj.getEmp();
+        EmployeeDetails2 obj = new EmployeeDetails2();
+        String[] emp = obj.getEmp();
+        obj.printEmp(emp);
     }
 }
 
-class EmployeeDetails{
-    Scanner scan=new Scanner(System.in);
-    public String getName(String str){
+class EmployeeDetails {
+    Scanner scan = new Scanner(System.in);
+
+    public String getUser(String str) {
+        System.out.println(str);
         return scan.nextLine();
     }
 }
 
-class EmployeeDetails2 extends  EmployeeDetails{
-    String[] emp=new String[3];
-    public String[] getEmp(){
-        String[] emp=new String[3];
-        emp[0]=getName("Enter the name: ");
-        emp[1]=getName("Enter the age: ");
-        emp[2]=getName("Enter the DOB: ");
+class EmployeeDetails2 extends EmployeeDetails {
+    String[] emp = new String[3];
+
+    public String[] getEmp() {
+        emp[0] = getUser("Enter the name: ");
+        emp[1] = getUser("Enter the age: ");
+        emp[2] = getUser("Enter the DOB: ");
         return emp;
+    }
+
+    public void printEmp(String[] emp) {
+        System.out.println("Name: " + emp[0]);
+        System.out.println("age: " + emp[1]);
+        System.out.println("DOB: " + emp[2]);
     }
 
 }

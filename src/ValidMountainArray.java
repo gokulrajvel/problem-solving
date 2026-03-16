129d3b5d6beea3b7fb85111094317e2464a721b9
Gokulraj Velu <vgokulraj360@gmail.com>
2026-03-16 11:29:48 +0530

    Easy 941. Valid Mountain Array

---
+++ src/ValidMountainArray.java
@@ -0,0 +1,22 @@
+public class ValidMountainArray {
+    public static void main(String[] args) {
+        ValidMountainArray obj = new ValidMountainArray();
+        System.out.println(obj.checkValidMountainArray(new int[] {0,3,2,1}));
+    }
+    boolean checkValidMountainArray(int[] arr){
+        if(arr.length<3){
+            return false;
+        }
+        int i=0;
+        while(i+1<arr.length&&arr[i]<arr[i+1]){
+            i++;
+        }
+        if(i==0||i==arr.length-1){
+            return false;
+        }
+        while (i+1<arr.length&&arr[i]>arr[i+1]){
+            i++;
+        }
+        return i==arr.length-1;
+    }
+}

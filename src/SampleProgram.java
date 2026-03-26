58ccccfcf7992ec3d5481b7203a4a6ec94fcee5c
Gokulraj Velu <vgokulraj360@gmail.com>
2026-03-26 20:06:15 +0530

    Completed the base class for Demo

--- src/SampleProgram.java
+++ src/SampleProgram.java
@@ -1,19 +1,32 @@
-import com.hackbase.ScannerClass;
-
+import com.hackbase.BaseScanner;
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
+import static java.util.Arrays.asList;
 
 public class SampleProgram {
     public static void main(String[] args) {
-        ScannerClass obj=new ScannerClass();
-        int len=obj.getInt();
-        String str= obj.getString();
-        System.out.println(len);
-        System.out.println(str);
-        int[] arr=obj.getIntArray(len);
-        obj.mergeSort(arr,0,arr.length-1);
-        String[] arr1=obj.getStringArray(len);
-        obj.displayArray(arr1);
-        System.out.println(Arrays.toString(arr));
+        BaseScanner sc=new BaseScanner();
+        System.out.println("--------------------Enter Number--------------------");
+        int val = sc.getInt();
+        System.out.println("--------------------Enter String--------------------");
+        String str = sc.getString();
+        System.out.println("--------------------Enter Char----------------------");
+        char ch = sc.getChar();
+        System.out.println("--------------------Enter Array Len-----------------");
+        int len = sc.getInt();
+        System.out.println("--------------------Enter Array Values--------------");
+        int[] arr = sc.getIntArray(len);
+        System.out.println("--------------------Printing the Array Values-------");
+        System.out.println(sc.displayArray(arr));
+        Integer[] list = new Integer[]{1, 2, 3, 4, 5};
+        System.out.println(sc.displayArray(list));
+        System.out.println("--------------------Enter Array Len for String------");
+        int val2 = sc.getInt();
+        System.out.println("--------------------Enter Array Values--------------");
+        String[] arr2 = sc.getStringArray(val2);
+        System.out.println("--------------------Printing the Array Values-------");
+        System.out.println(sc.displayArray(arr2));
+
     }
 }

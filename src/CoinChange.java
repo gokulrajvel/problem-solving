e47f2507447693ba5c24a7234a647fa16a909879
Gokulraj Velu <vgokulraj360@gmail.com>
2026-03-27 14:58:39 +0530

    Medium 322. Coin Change

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0)
+            return 0;
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (coin <= i && dp[i - coin] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+        if (dp[amount] == Integer.MAX_VALUE)
+            return -1;
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        CoinChange cc = new CoinChange();
+        int[] arr = {1, 2, 5};
+        System.out.println(cc.coinChange(arr, 11));
+    }
 }

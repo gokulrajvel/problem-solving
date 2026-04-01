df3f658a9a5cfa100a86ba759a19dd02843671f6
Gokulraj Velu <vgokulraj360@gmail.com>
2026-04-01 09:57:28 +0530

    medium 45. Jump Game II

--- src/JumpGameII.java
+++ src/JumpGameII.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class JumpGameII {
+    public static void main(String[] args) {
+        JumpGameII jg = new JumpGameII();
+        System.out.println(jg.jump(new int[]{2,3,1,1,4}));
+    }
+    public int jump(int[] nums) {
+        int maxReach = 0,count=0,currentEnd=0;
+        for(int i=0;i<nums.length-1;i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if(i==currentEnd){
+                count++;
+                currentEnd=maxReach;
+            }
+        }
+        return count;
+    }
 }

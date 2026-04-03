337af2745022d9c9905271b343272bfb75444c01
pain <vgokulraj360@gmail.com>
2026-04-03 19:39:07 +0530

    medium 57. Insert Interval.

---
+++ src/InsertInterval.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InsertInterval {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        InsertInterval obj = new InsertInterval();
+        int[][] intervals = new int[][]{{1, 2},
+                {3, 5},
+                {6, 7},
+                {8, 10},
+                {12, 16},
+                {1, 2},
+                {3, 5},
+                {6, 7},
+                {8, 10},
+                {12, 16}};
+        int[] interval = new int[]{4,8};
+        System.out.println(Arrays.deepToString(obj.insert(intervals, interval)));
+    }
+}
